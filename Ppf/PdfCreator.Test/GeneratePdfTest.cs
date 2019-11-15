3917b214dd349041e59e2a4bb7d9cab4b697fa61
Jerson brito <jersonlbrito2017@gmail.com>
2019-11-15 11:25:33 -0300

    to merge

--- Ppf/PdfCreator.Test/GeneratePdfTest.cs
+++ Ppf/PdfCreator.Test/GeneratePdfTest.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace PdfGenerator.Test
 {
@@ -24,6 +26,48 @@ namespace PdfGenerator.Test
             }
 
             Assert.IsTrue(File.Exists(path));
+        }
+
+        [TestMethod]
+        public void GenerateCertify()
+        {
+            var list = new List<string>
+            {
+                "Aleixo López",
+                "André Parreira",
+                "Bernardete Gracia",
+                "Ermelinda Valentín",
+                "Filinto Themes",
+                "Rodolfo Gama",
+                "Sancho Montero",
+                "Socorro Botelho",
+                "Virgílio Cairu",
+                "Virgílio Pegado"
+            };
+
+            var pathRoot = @"..\..\..\test\";
+            var pathPhoto = @"..\..\..\test\image_certicate.jpg";
+
+            list.ToList().ForEach(name =>
+            {
+                var pathFile = $"{pathRoot}{name.ToUpper().Trim().Replace(" ", "_")}.pdf";
+
+                if (File.Exists(pathFile))
+                {
+                    File.Delete(pathFile);
+                }
+
+                using (var fs = new FileStream(pathFile, FileMode.Create))
+                using (var ms = PdfGenerator.Generator.Certificados.Generate(name,pathPhoto))
+                {
+                    ms.CopyTo(fs);
+                    fs.CopyTo(ms);
+
+                    Assert.IsTrue(File.Exists(pathFile));
+                }
+
+            });
+
         }
     }
 }
