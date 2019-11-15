51bd472d4fbd8b3c8a8c71780c6230fc44f0cc15
Jerson brito <jersonlbrito2017@gmail.com>
2019-11-15 08:27:58 -0300

    completo

--- Ppf/PdfCreator.Test/GeneratePdfTest.cs
+++ Ppf/PdfCreator.Test/GeneratePdfTest.cs
@@ -1,29 +1,53 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace PdfGenerator.Test
 {
     [TestClass]
     public class GeneratePdfTest
     {
+        //[TestMethod]
+        //public void GenerateTest()
+        //{
+        //    const string path = @"..\..\..\test\test.pdf";
+
+        //    if (File.Exists(path))
+        //    {
+        //        File.Delete(path);
+        //    }
+
+        //    using (var ms = PdfGenerator.Generator.DocumentGenerator.Generate())
+        //    using (var fs = new FileStream(path, FileMode.Create))
+        //    {
+        //        ms.CopyTo(fs);
+        //        fs.CopyTo(ms);
+        //    }
+
+        //    Assert.IsTrue(File.Exists(path));
+        //}
+
         [TestMethod]
-        public void GenerateTest()
+        public void GenerateCertify()
         {
-            const string path = @"..\..\..\test\test.pdf";
+          
+            var list = File.ReadAllLines(@"C:\Users\jerso\Downloads\PRODEPE\lista.txt", Encoding.Default);
 
-            if (File.Exists(path))
+            list.ToList().ForEach(x =>
             {
-                File.Delete(path);
-            }
+                using (var ms = PdfGenerator.Generator.Certificados.Generate(x.Trim()))
+                using (var fs = new FileStream(@"C:\Users\jerso\Downloads\PRODEPE\CERTIFICADOS ASSINADOS\" + x.ToUpper().Trim().Replace(" ","_") + ".pdf", FileMode.Create))
+                {
+                    ms.CopyTo(fs);
+                    fs.CopyTo(ms);
+                }
 
-            using (var ms = PdfGenerator.Generator.DocumentGenerator.Generate())
-            using (var fs = new FileStream(path, FileMode.Create))
-            {
-                ms.CopyTo(fs);
-                fs.CopyTo(ms);
-            }
+            });
 
-            Assert.IsTrue(File.Exists(path));
+
+
+
         }
     }
 }
---
+++ Ppf/PdfGenerator/Generator/Certificados.cs
@@ -0,0 +1,38 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using PdfGenerator.Util;
+using System.IO;
+
+namespace PdfGenerator.Generator
+{
+   public class Certificados
+    {
+
+        protected Certificados()
+        {
+        }
+
+        public static MemoryStream Generate(string x)
+        {
+            PdfHelper pdfElemment;
+            var ms = new MemoryStream();
+
+            using (var doc = new Document()) { 
+            doc.SetPageSize(PageSize.A4.Rotate());
+            doc.SetMargins(0, 0, 0, 0);
+            pdfElemment = new PdfHelper(doc, ms);
+            var image = Image.GetInstance(@"C:\Users\jerso\Downloads\PRODEPE\Prodepe Assinado-01.png");
+            pdfElemment.AddImage(image, 0, 0, doc.PageSize.Width, doc.PageSize.Height + 3);
+
+            pdfElemment.TextCenter(x, BaseFont.HELVETICA_BOLD, 25, pdfElemment.CenterX(), 350f);
+
+            doc.Close();
+            var msInfo = ms.ToArray();
+            ms.Write(msInfo, 0, msInfo.Length);
+
+            ms.Position = 0;
+            return ms;
+            }
+        }
+    }
+}
